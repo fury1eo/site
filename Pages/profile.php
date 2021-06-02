84d6ed6da062e84e34782f2a29dcc6999a063ca5
Dorialean <lana.lakutina@mail.ru>
2021-06-02 09:51:47 +0300

    adding css

---
+++ Pages/profile.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Allstyles/lkstyle.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
+    <title>Личный кабинет</title>
+</head>
+<body>
+    <header>
+        <div class="navbar">
+        <a href="index.html"><span class="main">Главная</span></a>
+            <span class="course">Курсы</span>
+            <span class="logout">Выйти</span>
+        </div>
+        <div class="profile">
+            <div class="photo">
+                <img
+                    src="Photo.png"
+                >
+            </div>
+            <div class="column1">
+            </div>
+            <!-- Здесь имя и фамилия должны быть заменены!-->
+            <span class="profilename">
+                Фамилия имя
+            </span>
+            <!-- Здесь должна быть дата рождения!-->
+            <span class="dob">
+                Дата рождения:
+            </span>
+            <span  class="sex">
+                Пол:
+            </span>
+            <div class="column2">
+            </div>
+            <span class="progress">
+                Прогресс
+            </span>
+            <span class="progressbar">
+                Курс пройден на n%
+            </span>
+            <span class="points">
+                Баллы за экзамен: n
+            </span>
+        </div>
+    </header>
+</body>
+</html>
