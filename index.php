a70772e6d37bfc8b07c77f01b360ab942c824af2
Dorialean <lana.lakutina@mail.ru>
2021-06-02 11:19:09 +0300

    new index with css and pictures

---
+++ index.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Allstyles/indexstyles.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
+    <title>Джентльмены</title>
+</head>
+<body>
+    <img
+        class="font"
+        src="img/main_bg.jpg"
+        alt="Фон с джентльменом"
+    >
+    <header>
+        <a href="index.html">
+            <img class="logo"
+            src="img/Logo.png"
+            title="Лого"
+            alt="Лого с галстуком"
+        >
+        </a>
+        <div class="navbar">
+            <a href="index.html"><span class="main">Главная</span></a>
+            <a href="#link"> <span class="service">О сервисе</span> </a>
+            <span class="course">Курс</span>
+            <span class="faq">FAQ</span>
+            <!-- Кнопка должна меняться на картинку и данные пользователя!-->
+            <div>
+                <a href="registration.html">
+                <button class="regbut">Зарегистрироваться</button>
+                </a>
+            </div>
+            <span> 
+                <img
+                    class="user"
+                    src="icons/User.png"
+                >
+            </span>
+            <div class="lk">
+                Фамилия <br>
+                Имя
+            </div>
+        </div>
+    </header>
+    <div class="maintext">
+        Джентльмен - это человек, общаясь 
+        с которым чувствуешь себя джентльменом
+    </div>
+    <div>
+        <a href="registration.html">
+        <button class="begin"> Начать обучаться </button>
+        </a>
+    </div>
+    <div id="link" class="line1"> </div>
+    <div class="who"> Кто мы?</div>
+    <div class="line2"> </div>
+    <div class="answer">
+        Наша онлайн-платформа поможет 
+        Вам стать истинным джентльменом, 
+        научит правильным манерам и поведению в 
+        приличных заведениях. Мы хотим сделать 
+        мир лучше!
+    </div>
+    <div class="step1">Шаг1: Зарегистрируйтесь!
+    </div>
+    <span class="regimg"> 
+        <img
+            src="img/step1.png"
+        >
+    </span>
+    <div class="step2">Шаг2: Обучайтесь!
+    </div>
+    <span class="learnimg"> 
+        <img
+            src="img/step2.png"
+        >
+    </span>
+    <div class="step3">Шаг3: Проверяйте знания!
+    </div>
+    <span class="checkimg"> 
+        <img
+            src="img/step3 (1).png"
+        >
+    </span>
+    <div class="step4">Шаг4: Отслеживайте прогресс!
+    </div>
+    <span class="progressimg"> 
+        <img
+            src="img/step4.png"
+        >
+    </span>
+    <div class="footer">
+        <a href="index.html">
+            <img class="logo2"
+            src="img/Logo2.png"
+            title="Лого"
+            alt="Лого с галстуком"
+            >
+        </a>
+        <div class="navbar2">
+            <a href="index.html"><span class="main2">Главная</span></a>
+            <a href="#link"><span class="service2">О сервисе</span></a>
+            <span class="course2">Курс</span>
+            <span class="faq2">FAQ</span>
+        </div>
+        <div class="icons">
+            <span class="vk">
+                <img
+                    src="icons/vk.png"
+                >
+            </span>
+            <span class="inst">
+                <img
+                    src="icons/instagram.png"
+                >
+            </span>
+            <span class="facebook">
+                <img
+                    src="icons/facebook.png"
+                >
+            </span>
+            <span class="telega">
+                <img
+                    src="icons/telegram.png"
+                >
+            </span>
+        </div>
+    </div>
+</body>
+</html>
